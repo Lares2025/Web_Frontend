a58a88a481e23cf270317c1000328a53eaeb9df7
E-jiwoo <livei07@naver.com>
2025-04-08 14:56:00 +0900

    add :: Login input - 로그인을 위해 필요한 이름, 아이디, 비밀번호, 주소를 적는 부분

--- src/pages/VueLogin.vue
+++ src/pages/VueLogin.vue
@@ -1,7 +1,19 @@
 <template>
   <div :style="Container">
-    <div :style="LogoTitle">LARES</div>
-    <div :style="LogoSub">BSSM - Logistics Autonomous Robot ERP System</div>
+    <div :style="LogoContain">
+      <div :style="LogoTitle">LARES</div>
+      <div :style="LogoSub">BSSM - Logistics Autonomous Robot ERP System</div>
+    </div>
+    <div :style="LoginContain">
+      <div>회원 가입</div>
+      <div>로그인</div>
+    </div>
+    <div :style="InputContain">
+      <input placeholder="이름" :style="LoginInput" class="logininput" />
+      <input placeholder="아이디" :style="LoginInput" class="logininput" />
+      <input placeholder="비밀번호" :style="LoginInput" class="logininput" />
+      <input placeholder="주소" :style="LoginInput" class="logininput" />
+    </div>
   </div>
 </template>
 
@@ -17,6 +29,15 @@ export default {
         bottom: 0,
         width: "100vw",
         height: "100vh",
+        display: "flex",
+        justifyContent: "center",
+        aliginItems: "center",
+      },
+      LogoContain: {
+        position: "fixed",
+        top: "85px",
+        display: "flex",
+        flexDirection: "column",
       },
       LogoTitle: {
         color: "#0C007B",
@@ -31,6 +52,34 @@ export default {
         fontFamily: "PretendardLight",
         fontWeight: "300",
         wordWrap: "break-word",
+        marginTop: "-10.2px",
+      },
+      LoginContain: {
+        position: "fixed",
+        top: "241px",
+        display: "flex",
+        flexDirection: "row",
+        gap: "40px",
+        color: "#0C007B",
+        fontSize: "20px",
+        fontFamily: "PretendardLight",
+      },
+      LoginInput: {
+        width: "428px",
+        height: "24px",
+        padding: "12px 10px", //위아래, 왼쪽오른쪽
+        border: "1px solid #818181",
+        borderRadius: "10px",
+        fontFamily: "PretendardThin",
+        fontSize: "20px",
+        backgroundColor: "#F6F6F6",
+      },
+      InputContain: {
+        position: "fixed",
+        top: "305px",
+        display: "flex",
+        flexDirection: "column",
+        gap: "20px",
       },
     };
   },
@@ -42,5 +91,11 @@ html,
 body {
   margin: 0;
   padding: 0;
+}
+
+.logininput::placeholder {
+  font-family: Pretendard;
+  font-weight: 100;
+  font-size: 20px;
 }
 </style>
