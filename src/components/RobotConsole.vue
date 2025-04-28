7630e7e5573e84a37f22745c0847d5054e095771
E-jiwoo <livei07@naver.com>
2025-04-28 19:50:27 +0900

    add :: Console / Count DataNumber - 콘솔부분 데이터 개수 세기

--- src/components/RobotConsole.vue
+++ src/components/RobotConsole.vue
@@ -1,6 +1,11 @@
 <template>
   <div :style="Container">
     <div :style="ConnectBox">
+      <input type="number" placeholder="데이터 개수" :style="DataNumber" />
+      <select v-model="selectedFruit" :style="DataArray">
+        <option value="up">오름차순</option>
+        <option value="down">내림차순</option>
+      </select>
       <div :style="Line" />
     </div>
   </div>
@@ -32,6 +37,31 @@ export default {
       },
       Line: {
         width: "100%",
+        height: "1px",
+        backgroundColor: "#CECCE5",
+        margin: "0px 12px",
+      },
+      DataNumber: {
+        width: "80px",
+        height: "14px",
+        padding: "12px 10px",
+        border: "1px solid #CECCE5",
+        borderRadius: "10px",
+        fontFamily: "PretendardThin",
+        fontsize: "12px",
+        color: "#818181",
+        backgroundColor: "#C5C5C5",
+      },
+      DataArray: {
+        width: "130px",
+        height: "39px",
+        padding: "12px 10px",
+        border: "1px solid #CECCE5",
+        borderRadius: "10px",
+        fontFamily: "PretendardThin",
+        fontsize: "12px",
+        color: "#818181",
+        backgroundColor: "#C5C5C5",
       },
     };
   },
