d9e25124006b7fff9281c54daee98a9dba0386b2
E-jiwoo <livei07@naver.com>
2025-05-12 14:55:01 +0900

    세 컴포넌트 코드 한 파일에 합침

--- src/components/RobotConsole.vue
+++ src/components/RobotConsole.vue
@@ -1,12 +1,23 @@
 <template>
   <div :style="Container">
     <div :style="ConnectBox">
-      <input type="number" placeholder="데이터 개수" :style="DataNumber" />
-      <select v-model="selectedFruit" :style="DataArray">
-        <option value="up">오름차순</option>
-        <option value="down">내림차순</option>
-      </select>
-      <div :style="Line" />
+      <div :style="Contain">
+        <div :style="Box1">
+          <div :style="Box2">
+            <input
+              type="number"
+              placeholder="데이터 개수"
+              :style="DataNumber"
+            />
+            <select v-model="selectedFruit" :style="DataArray">
+              <option value="up">오름차순</option>
+              <option value="down">내림차순</option>
+            </select>
+            <button>제어 전송</button>
+          </div>
+        </div>
+        <div :style="Line" />
+      </div>
     </div>
   </div>
 </template>
@@ -35,11 +46,29 @@ export default {
         border: "1px solid #818181",
         borderRadius: "15px",
       },
+      Contain: {
+        width: "100%",
+        height: "100%",
+        display: "flex",
+        flexDirection: "column",
+        gap: "12px",
+      },
+      Box1: {
+        display: "flex",
+        flexDirection: "row",
+        gap: "12px",
+      },
+      Box2: {
+        width: "100%",
+        height: "100%",
+        display: "flex",
+        flexDirection: "row",
+        justifyContent: "space-between",
+      },
       Line: {
         width: "100%",
         height: "1px",
         backgroundColor: "#CECCE5",
-        margin: "0px 12px",
       },
       DataNumber: {
         width: "80px",
@@ -48,20 +77,21 @@ export default {
         border: "1px solid #CECCE5",
         borderRadius: "10px",
         fontFamily: "PretendardThin",
-        fontsize: "12px",
+        fontSize: "12px",
         color: "#818181",
         backgroundColor: "#C5C5C5",
       },
       DataArray: {
-        width: "175px",
+        width: "100px",
         height: "39px",
         padding: "10px 10px",
         border: "1px solid #CECCE5",
         borderRadius: "10px",
         fontFamily: "PretendardThin",
-        fontsize: "12px",
+        fontSize: "12px",
         color: "#818181",
         backgroundColor: "#C5C5C5",
+        margin: "0px 280px 0px 0px",
       },
     };
   },
--- src/pages/main/DataSend.vue
+++ src/pages/main/DataSend.vue
@@ -1,50 +1,325 @@
 <template>
   <div>
-    <RobotConnect />
-    <RobotCam />
-    <RobotConsole />
+    <!-- 연결 여부 박스 -->
+    <div :style="containerTop">
+      <div :style="connectBoxTop">
+        <img :src="Img" alt="robotimg" :style="robotImg" />
+        <div>
+          <div :style="status">
+            <div :style="computedStatusDot" />
+            <div :style="statusTextStyle">{{ statusText }}</div>
+          </div>
+          <div :style="connect">
+            <div :style="name">LARES 01</div>
+            <button :style="connectBtn" @click="toggleConnection">
+              {{ buttonText }}
+            </button>
+          </div>
+          <div :style="address">
+            <input
+              type="text"
+              maxlength="3"
+              placeholder="255"
+              v-model="address1"
+              @input="onInput('address1', $event)"
+              :style="addressInput"
+            />
+            <div :style="addressDot">.</div>
+            <input
+              type="text"
+              maxlength="3"
+              placeholder="255"
+              v-model="address2"
+              @input="onInput('address2', $event)"
+              :style="addressInput"
+            />
+            <div :style="addressDot">.</div>
+            <input
+              type="text"
+              maxlength="3"
+              placeholder="255"
+              v-model="address3"
+              @input="onInput('address3', $event)"
+              :style="addressInput"
+            />
+            <div :style="addressDot">.</div>
+            <input
+              type="text"
+              maxlength="3"
+              placeholder="255"
+              v-model="address4"
+              @input="onInput('address4', $event)"
+              :style="addressInput"
+            />
+          </div>
+        </div>
+      </div>
+    </div>
+
+    <!-- 콘솔 박스 -->
+    <div :style="containerMiddle">
+      <div :style="connectBoxMiddle">
+        <div :style="contain">
+          <div :style="box1">
+            <div :style="box2">
+              <input
+                type="number"
+                placeholder="데이터 개수"
+                :style="dataNumber"
+              />
+              <select v-model="selectedOrder" :style="dataArray">
+                <option value="up">오름차순</option>
+                <option value="down">내림차순</option>
+              </select>
+              <button>제어 전송</button>
+            </div>
+          </div>
+          <div :style="line" />
+        </div>
+      </div>
+    </div>
+
+    <!-- 웹캠 박스 -->
+    <div :style="containerRight">
+      <div :style="connectBoxRight"></div>
+    </div>
   </div>
 </template>
 
 <script>
-import RobotConnect from "@/components/RobotConnect.vue";
-import RobotCam from "@/components/RobotCam.vue";
-import RobotConsole from "@/components/RobotConsole.vue";
-
 export default {
-  name: "DateSend",
-  components: {
-    RobotConnect,
-    RobotCam,
-    RobotConsole,
-  },
   data() {
     return {
-      Container: {
+      // 연결 여부 박스
+      containerTop: {
         position: "absolute",
         top: 0,
         left: 0,
         right: 0,
-        bottom: 0,
-        width: "100vw",
-        height: "100vh",
+        bottom: "250px",
         display: "flex",
         justifyContent: "center",
         alignItems: "center",
       },
-      Contain_col: {
-        position: "relative",
+      connectBoxTop: {
+        position: "fixed",
+        display: "flex",
+        gap: "30px",
+        width: "492px",
+        height: "124px",
+        padding: "60px",
+        border: "1px solid #818181",
+        borderRadius: "15px",
+      },
+      Img: require("@/assets/robot.svg"),
+      robotImg: {
+        width: "120px",
+        height: "120px",
+      },
+      status: {
+        display: "flex",
+        alignItems: "center",
+        flexDirection: "row",
+        gap: "6px",
+      },
+      statusTextStyle: {
+        color: "black",
+        fontSize: "18px",
+        fontFamily: "PretendardLight",
+      },
+      connect: {
+        display: "flex",
+        alignItems: "center",
+        gap: "120px",
+        paddingTop: "6px",
+      },
+      name: {
+        color: "black",
+        fontSize: "28px",
+        fontFamily: "GongGothicBold",
+      },
+      address: {
+        display: "flex",
+        gap: "10px",
+        alignItems: "flex-end",
+        paddingTop: "16px",
+      },
+      addressInput: {
+        width: "36px",
+        height: "24px",
+        padding: "12px 14px",
+        border: "1px solid #CECCE5",
+        borderRadius: "10px",
+        fontSize: "20px",
+        fontFamily: "PretendardLight",
+        backgroundColor: "#F6F6F6",
+      },
+      addressDot: {
+        color: "#818181",
+        fontSize: "20px",
+        fontFamily: "PretendardLight",
+      },
+      address1: "",
+      address2: "",
+      address3: "",
+      address4: "",
+      connected: false,
+
+      // 콘솔 박스
+      containerMiddle: {
+        position: "absolute",
+        top: "380px",
+        left: 0,
+        right: "640px",
+        bottom: 0,
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+      },
+      connectBoxMiddle: {
+        position: "fixed",
+        display: "flex",
+        gap: "30px",
+        width: "572px",
+        height: "278px",
+        padding: "20px",
+        border: "1px solid #818181",
+        borderRadius: "15px",
+      },
+      contain: {
+        width: "100%",
+        height: "100%",
         display: "flex",
         flexDirection: "column",
-        gap: "60px",
+        gap: "12px",
       },
-      Contain_row: {
-        position: "relative",
+      box1: {
         display: "flex",
         flexDirection: "row",
-        gap: "100px",
+        gap: "12px",
+      },
+      box2: {
+        width: "100%",
+        height: "100%",
+        display: "flex",
+        flexDirection: "row",
+        justifyContent: "space-between",
+      },
+      line: {
+        width: "100%",
+        height: "1px",
+        backgroundColor: "#CECCE5",
+      },
+      selectedOrder: "up",
+
+      // 웹캠 박스
+      containerRight: {
+        position: "absolute",
+        top: "380px",
+        left: "640px",
+        right: 0,
+        bottom: 0,
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+      },
+      connectBoxRight: {
+        position: "fixed",
+        display: "flex",
+        gap: "30px",
+        width: "492px",
+        height: "278px",
+        padding: "20px 60px",
+        border: "1px solid #818181",
+        borderRadius: "15px",
       },
     };
+  },
+  computed: {
+    isAddressComplete() {
+      return (
+        this.address1 !== "" &&
+        this.address2 !== "" &&
+        this.address3 !== "" &&
+        this.address4 !== ""
+      );
+    },
+    buttonText() {
+      if (!this.isAddressComplete) return "연결";
+      return this.connected ? "취소" : "연결";
+    },
+    connectBtn() {
+      if (!this.isAddressComplete) {
+        return {
+          padding: "10px 22px",
+          border: "none",
+          borderRadius: "6px",
+          backgroundColor: "#818181",
+          color: "#C5C5C5",
+          fontSize: "12px",
+          fontFamily: "PretendardBold",
+          cursor: "not-allowed",
+        };
+      }
+
+      return {
+        padding: "10px 22px",
+        border: "none",
+        borderRadius: "6px",
+        backgroundColor: this.connected ? "#F6F6F6" : "#0C007B",
+        color: this.connected ? "#0C007B" : "white",
+        fontSize: "12px",
+        fontFamily: "PretendardBold",
+        cursor: "pointer",
+      };
+    },
+    computedStatusDot() {
+      return {
+        width: "12px",
+        height: "12px",
+        borderRadius: "300px",
+        backgroundColor: this.connected ? "#00C851" : "#D70000",
+      };
+    },
+    statusText() {
+      return this.connected ? "Connect" : "Disconnect";
+    },
+    dataNumber() {
+      return {
+        width: "80px",
+        height: "14px",
+        padding: "12px 10px",
+        border: "1px solid #CECCE5",
+        borderRadius: "10px",
+        fontFamily: "PretendardThin",
+        fontSize: "12px",
+        color: "#818181",
+        backgroundColor: this.connected ? "#F6F6F6" : "#C5C5C5",
+      };
+    },
+    dataArray() {
+      return {
+        width: "100px",
+        height: "39px",
+        padding: "10px 10px",
+        border: "1px solid #CECCE5",
+        borderRadius: "10px",
+        fontFamily: "PretendardThin",
+        fontSize: "12px",
+        color: "#818181",
+        backgroundColor: this.connected ? "#F6F6F6" : "#C5C5C5",
+        margin: "0px 280px 0px 0px",
+      };
+    },
+  },
+  methods: {
+    onInput(field, event) {
+      this[field] = event.target.value.replace(/[^0-9]/g, "").slice(0, 3);
+    },
+    toggleConnection() {
+      if (!this.isAddressComplete) return;
+      this.connected = !this.connected;
+    },
   },
 };
 </script>
