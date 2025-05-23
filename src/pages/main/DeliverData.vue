e3d1b95d52f12f75c8e86b386e0364b256db8d42
E-jiwoo <livei07@naver.com>
2025-05-23 09:46:12 +0900

    add :: deliver axios - deliver 페이지 axios 연동

--- src/pages/main/DeliverData.vue
+++ src/pages/main/DeliverData.vue
@@ -17,8 +17,9 @@
 </template>
 
 <script>
-import { ref } from "vue";
+import { ref, onMounted } from "vue";
 import { AgGridVue } from "ag-grid-vue3";
+import axios from "axios";
 
 export default {
   name: "App",
@@ -26,54 +27,32 @@ export default {
     AgGridVue,
   },
   setup() {
-    const rowData = ref([
-      {
-        state: true,
-        number: "1",
-        orderDate: "25년 05월 09일",
-        postId: "0.0_wji",
-        getId: "sliver",
-        address: "부산광역시 강서구 가락대로 1393",
-        item: "라즈베리파이 / 1",
-        memo: "안전 배송",
-        deliverDate: "25년 5월 30일",
-      },
-      {
-        state: true,
-        number: "2",
-        orderDate: "25년 06월 13일",
-        postId: "jiwoo",
-        getId: "tomato",
-        address: "부산광역시 강서구 가락대로 1393",
-        item: "솔레노이드 액추에이터 / 5",
-        memo: "신속 배송",
-        deliverDate: "25년 10월 30일",
-      },
-      {
-        state: true,
-        number: "3",
-        orderDate: "25년 07월 29일",
-        postId: "kangeun",
-        getId: "gold",
-        address: "부산광역시 강서구 가락대로 1393",
-        item: "물 / 10",
-        memo: "안전 배송",
-        deliverDate: "25년 10월 30일",
-      },
-    ]);
-
+    const rowData = ref([]);
     const colDefs = ref([
-      { field: "state" },
-      { field: "number" },
-      { field: "orderDate" },
-      { field: "postId" },
-      { field: "getId" },
-      { field: "address" },
-      { field: "item" },
-      { field: "memo" },
-      { field: "deliverDate" },
+      { field: "orderId", width: 80 },
+      { field: "orderCreatedAt", width: 150 },
+      { field: "sendUserId", width: 100 },
+      { field: "receiveUserId", width: 100 },
+      { field: "receiveUserAddress", width: 300 },
+      { field: "orderItem", width: 200 },
+      { field: "orderMemo", width: 190 },
+      { field: "deliveryDate", width: 150 },
     ]);
 
+    onMounted(async () => {
+      try {
+        const response = await axios.get("lares/api/order/");
+        console.log(response.data);
+        if (Array.isArray(response.data.orderInfoList)) {
+          rowData.value = response.data.orderInfoList;
+        } else {
+          console.error("받은 데이터가 배열이 아닙니다.");
+          rowData.value = [];
+        }
+      } catch (error) {
+        console.error("데이터 불러오기 실패:", error);
+      }
+    });
     return {
       rowData,
       colDefs,
@@ -89,7 +68,7 @@ export default {
         bottom: 0,
         display: "flex",
         justifyContent: "center",
-        alignitems: "center",
+        alignItems: "center",
       },
       Delete: {
         display: "flex",
@@ -114,13 +93,13 @@ export default {
       Contain: {
         width: "1276px",
         height: "476px",
-        border: "1px solpostId rgba(0, 0, 0, 0.5)",
+        border: "1px solid rgba(0, 0, 0, 0.5)",
         borderRadius: "12px",
       },
       Head: {
         height: "76px",
         display: "flex",
-        flexDirection: "robot",
+        flexDirection: "row",
         justifyContent: "space-between",
         alignitems: "center",
         fontFamily: "GongGothicMedium",
@@ -142,32 +121,6 @@ export default {
         width: "20px",
         height: "20px",
       },
-      robots: [
-        {
-          postId: "0001",
-          orderDate: "20250312",
-          manager: "0.0_wji",
-          robot: "LARES1",
-          count: 6,
-          method: "ASC",
-        },
-        {
-          postId: "0002",
-          orderDate: "20250313",
-          manager: "0.0_kim",
-          robot: "LARES2",
-          count: 8,
-          method: "DESC",
-        },
-        {
-          postId: "0003",
-          orderDate: "20250314",
-          manager: "0.0_lee",
-          robot: "LARES3",
-          count: 5,
-          method: "ASC",
-        },
-      ],
     };
   },
 };
