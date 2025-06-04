8ee69be6cd42530b267663453e2ef5d12b5a4d84
E-jiwoo <livei07@naver.com>
2025-06-04 13:52:23 +0900

    add :: header move to page - 헤더 페이지 이동

--- src/components/common/VueHeader.vue
+++ src/components/common/VueHeader.vue
@@ -8,7 +8,7 @@
         :class="['page', { active: selectedPage === index }]"
         @click="selectPage(index)"
       >
-        {{ page }}
+        {{ page.name }}
       </div>
     </div>
     <div class="logout">로그아웃</div>
@@ -21,16 +21,31 @@ export default {
     return {
       selectedPage: 0,
       pageList: [
-        "로봇 제어 데이터 전송",
-        "제어 내역 데이터 관리",
-        "배송 물품 데이터 관리",
-        "기타 데이터 관리",
+        { name: "로봇 제어 데이터 전송", path: "/send" },
+        { name: "제어 내역 데이터 관리", path: "/control" },
+        { name: "배송 물품 데이터 관리", path: "/deliver" },
+        { name: "기타 데이터 관리", path: "/etc" },
       ],
     };
   },
+
   methods: {
     selectPage(index) {
       this.selectedPage = index;
+      const selectedPath = this.pageList[index].path;
+      this.$router.push(selectedPath);
+    },
+    updateSelectedPageByPath(path) {
+      const foundIndex = this.pageList.findIndex((page) => page.path === path);
+      this.selectedPage = foundIndex !== -1 ? foundIndex : 0;
+    },
+  },
+  mounted() {
+    this.updateSelectedPageByPath(this.$route.path);
+  },
+  watch: {
+    "$route.path"(newPath) {
+      this.updateSelectedPageByPath(newPath);
     },
   },
 };
@@ -42,6 +57,7 @@ export default {
   left: 0;
   right: 0;
   top: 0;
+  z-index: 1000;
   padding: 0 80px;
   height: 85px;
   display: flex;
