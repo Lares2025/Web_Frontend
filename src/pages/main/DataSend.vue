7145497549b8f035af74dbb7ca2577251fdd29f4
E-jiwoo <livei07@naver.com>
2025-07-17 12:21:05 +0900

    add :: axios img - 이미지 불러오기기

--- src/pages/main/DataSend.vue
+++ src/pages/main/DataSend.vue
@@ -1,61 +1,5 @@
 <template>
-  <div>
-    <!-- 연결 여부 박스 -->
-    <div :style="containerTop">
-      <div :style="connectBoxTop">
-        <img :src="Img" alt="robotimg" :style="robotImg" />
-        <div>
-          <div :style="status">
-            <div :style="computedStatusDot" />
-            <div :style="statusTextStyle">{{ statusText }}</div>
-          </div>
-          <div :style="connect">
-            <div :style="name">LARES 01</div>
-            <button :style="connectBtn" @click="toggleConnection">
-              {{ buttonText }}
-            </button>
-          </div>
-          <div :style="address">
-            <input
-              type="text"
-              maxlength="3"
-              placeholder="255"
-              v-model="address1"
-              @input="onInput('address1', $event)"
-              :style="addressInput"
-            />
-            <div :style="addressDot">.</div>
-            <input
-              type="text"
-              maxlength="3"
-              placeholder="255"
-              v-model="address2"
-              @input="onInput('address2', $event)"
-              :style="addressInput"
-            />
-            <div :style="addressDot">.</div>
-            <input
-              type="text"
-              maxlength="3"
-              placeholder="255"
-              v-model="address3"
-              @input="onInput('address3', $event)"
-              :style="addressInput"
-            />
-            <div :style="addressDot">.</div>
-            <input
-              type="text"
-              maxlength="3"
-              placeholder="255"
-              v-model="address4"
-              @input="onInput('address4', $event)"
-              :style="addressInput"
-            />
-          </div>
-        </div>
-      </div>
-    </div>
-
+  <div :style="centerWrapper">
     <!-- 콘솔 박스 -->
     <div :style="containerMiddle">
       <div :style="connectBoxMiddle">
@@ -78,16 +22,12 @@
         </div>
       </div>
     </div>
-
-    <!-- 웹캠 박스 -->
-    <div :style="containerRight">
-      <div :style="connectBoxRight">
-        <div :style="box3">
-          <button :style="actionBtn">DC 모터 동작 확인</button>
-          <button :style="actionBtn">전자석 모듈 동작 확인</button>
-          <button :style="actionBtn">카메라 동작 확인</button>
-        </div>
-        <video ref="webcam" autoplay playsinline :style="webcamStyle"></video>
+    <!-- 오른쪽 이미지 박스 -->
+    <div :style="imgBoxWrapper">
+      <button :style="loadBtn" @click="fetchImage">불러오기</button>
+      <div :style="imgContainer">
+        <img v-if="imgSrc" :src="imgSrc" :style="imgStyle" alt="제어 이미지" />
+        <div v-else :style="imgPlaceholder">이미지가 없습니다</div>
       </div>
     </div>
   </div>
@@ -97,94 +37,22 @@
 export default {
   data() {
     return {
-      // 연결 여부 박스
-      containerTop: {
-        position: "absolute",
-        top: 0,
-        left: 0,
-        right: 0,
-        bottom: "250px",
+      centerWrapper: {
         display: "flex",
         justifyContent: "center",
         alignItems: "center",
+        width: "100%",
+        height: "100%",
+        overflow: "hidden",
+        gap: "64px",
       },
-      connectBoxTop: {
-        position: "fixed",
-        display: "flex",
-        gap: "30px",
-        width: "492px",
-        height: "124px",
-        padding: "60px",
-        border: "1px solid #CECCE5",
-        borderRadius: "15px",
-      },
-      Img: require("@/assets/robot.svg"),
-      robotImg: {
-        width: "120px",
-        height: "120px",
-      },
-      status: {
-        display: "flex",
-        alignItems: "center",
-        flexDirection: "row",
-        gap: "6px",
-      },
-      statusTextStyle: {
-        color: "black",
-        fontSize: "18px",
-        fontFamily: "PretendardLight",
-      },
-      connect: {
-        display: "flex",
-        alignItems: "center",
-        gap: "120px",
-        paddingTop: "6px",
-      },
-      name: {
-        color: "black",
-        fontSize: "28px",
-        fontFamily: "GongGothicBold",
-      },
-      address: {
-        display: "flex",
-        gap: "10px",
-        alignItems: "flex-end",
-        paddingTop: "16px",
-      },
-      addressInput: {
-        width: "36px",
-        height: "24px",
-        padding: "12px 14px",
-        border: "1px solid #CECCE5",
-        borderRadius: "10px",
-        fontSize: "20px",
-        fontFamily: "PretendardLight",
-        backgroundColor: "#F6F6F6",
-      },
-      addressDot: {
-        color: "#818181",
-        fontSize: "20px",
-        fontFamily: "PretendardLight",
-      },
-      address1: "",
-      address2: "",
-      address3: "",
-      address4: "",
-      connected: false,
-
-      // 콘솔 박스
       containerMiddle: {
-        position: "absolute",
-        top: "370px",
-        left: 0,
-        right: "640px",
-        bottom: 0,
         display: "flex",
         justifyContent: "center",
         alignItems: "center",
       },
       connectBoxMiddle: {
-        position: "fixed",
+        position: "relative",
         display: "flex",
         gap: "30px",
         width: "572px",
@@ -218,84 +86,54 @@ export default {
         backgroundColor: "#CECCE5",
       },
       selectedOrder: "up",
-
-      // 웹캠 박스
-      containerRight: {
-        position: "absolute",
-        top: "370px",
-        left: "640px",
-        right: 0,
-        bottom: 0,
+      imgBoxWrapper: {
         display: "flex",
-        justifyContent: "center",
+        flexDirection: "column",
         alignItems: "center",
-      },
-      connectBoxRight: {
-        position: "fixed",
-        width: "612px",
-        height: "318px",
+        justifyContent: "center",
+        width: "350px",
+        height: "278px",
         border: "1px solid #CECCE5",
         borderRadius: "15px",
+        background: "#fff",
+        gap: "16px",
+        boxSizing: "border-box",
       },
-      webcamStyle: {
-        width: "100%",
-        height: "100%",
+      loadBtn: {
+        width: "120px",
+        height: "40px",
+        borderRadius: "8px",
+        border: "none",
+        background: "#0C007B",
+        color: "#fff",
+        fontFamily: "PretendardBold",
+        fontSize: "15px",
+        cursor: "pointer",
+      },
+      imgContainer: {
+        width: "300px",
+        height: "180px",
+        display: "flex",
+        alignItems: "center",
+        justifyContent: "center",
+        background: "#F6F6F6",
         borderRadius: "10px",
-        objectFit: "cover",
+        overflow: "hidden",
       },
-      box3: {
-        position: "absolute",
+      imgStyle: {
+        maxWidth: "100%",
+        maxHeight: "100%",
+        objectFit: "contain",
       },
+      imgPlaceholder: {
+        color: "#818181",
+        fontFamily: "PretendardLight",
+        fontSize: "16px",
+      },
+      imgSrc: null,
     };
   },
   computed: {
-    isAddressComplete() {
-      return (
-        this.address1 !== "" &&
-        this.address2 !== "" &&
-        this.address3 !== "" &&
-        this.address4 !== ""
-      );
-    },
-    buttonText() {
-      if (!this.isAddressComplete) return "연결";
-      return this.connected ? "취소" : "연결";
-    },
-    connectBtn() {
-      if (!this.isAddressComplete) {
-        return {
-          padding: "10px 22px",
-          border: "none",
-          borderRadius: "6px",
-          backgroundColor: "#818181",
-          color: "#C5C5C5",
-          fontSize: "12px",
-          fontFamily: "PretendardBold",
-          cursor: "not-allowed",
-        };
-      }
-      return {
-        padding: "10px 22px",
-        border: "none",
-        borderRadius: "6px",
-        backgroundColor: this.connected ? "#F6F6F6" : "#0C007B",
-        color: this.connected ? "#0C007B" : "white",
-        fontSize: "12px",
-        fontFamily: "PretendardBold",
-        cursor: "pointer",
-      };
-    },
-    computedStatusDot() {
-      return {
-        width: "12px",
-        height: "12px",
-        borderRadius: "300px",
-        backgroundColor: this.connected ? "#00C851" : "#D70000",
-      };
-    },
-    statusText() {
-      return this.connected ? "Connect" : "Disconnect";
-    },
     dataNumber() {
       return {
         width: "80px",
@@ -305,8 +143,8 @@ export default {
         borderRadius: "10px",
         fontFamily: "PretendardRegular",
         fontSize: "12px",
-        color: this.connected ? "#2C2C2C" : "#818181",
-        backgroundColor: this.connected ? "#F6F6F6" : "#C5C5C5",
+        color: "#2C2C2C",
+        backgroundColor: "#F6F6F6",
       };
     },
     dataArray() {
@@ -318,8 +156,8 @@ export default {
         borderRadius: "10px",
         fontFamily: "PretendardThin",
         fontSize: "12px",
-        color: this.connected ? "#2C2C2C" : "#818181",
-        backgroundColor: this.connected ? "#F6F6F6" : "#C5C5C5",
+        color: "#2C2C2C",
+        backgroundColor: "#F6F6F6",
         margin: "0px 250px 0px 0px",
       };
     },
@@ -332,8 +170,8 @@ export default {
         border: "none",
         fontFamily: "PretendardBold",
         fontSize: "12px",
-        color: this.connected ? "#FFFFFF" : "#C5C5C5",
-        backgroundColor: this.connected ? "#0C007B" : "#818181",
+        color: "#FFFFFF",
+        backgroundColor: "#0C007B",
       };
     },
     actionBtn() {
@@ -346,52 +184,51 @@ export default {
         border: "none",
         fontFamily: "PretendardBold",
         fontSize: "12px",
-        color: this.connected ? "#FFFFFF" : "#C5C5C5",
-        backgroundColor: this.connected ? "#0C007B" : "#818181",
+        color: "#FFFFFF",
+        backgroundColor: "#0C007B",
       };
     },
   },
   methods: {
-    onInput(field, event) {
-      this[field] = event.target.value.replace(/[^0-9]/g, "").slice(0, 3);
-    },
-    toggleConnection() {
-      if (!this.isAddressComplete) return;
-      this.connected = !this.connected;
-    },
-    startWebcam() {
-      navigator.mediaDevices
-        .getUserMedia({ video: true })
-        .then((stream) => {
-          this.$refs.webcam.srcObject = stream;
-        })
-        .catch((err) => {
-          console.error("웹캠 접근 실패:", err);
+    async fetchImage() {
+      try {
+        const response = await fetch("/lares/api/control/img", {
+          method: "GET",
+          headers: {
+            Accept: "image/jpeg",
+          },
         });
-    },
-    stopWebcam() {
-      const stream = this.$refs.webcam?.srcObject;
-      if (stream) {
-        stream.getTracks().forEach((track) => track.stop());
-        this.$refs.webcam.srcObject = null;
-      }
-    },
-  },
-  mounted() {},
-  watch: {
-    connected(newVal) {
-      if (newVal) {
-        this.startWebcam();
-      } else {
-        this.stopWebcam();
+        if (!response.ok) throw new Error("이미지 불러오기 실패");
+        const blob = await response.blob();
+        if (!blob.type.startsWith("image/")) {
+          const text = await blob.text();
+          console.log(text);
+          alert("이미지 대신 받은 데이터: " + text);
+          this.imgSrc = null;
+          return;
+        }
+        this.imgSrc = URL.createObjectURL(blob);
+      } catch (e) {
+        this.imgSrc = null;
+        alert("이미지 불러오기에 실패했습니다.");
       }
     },
   },
 };
 </script>
+
 <style scoped>
 input::placeholder {
   color: #818181;
   font-family: "PretendardThin";
+}
+</style>
+<style>
+html,
+body,
+#app {
+  height: 100%;
+  margin: 0;
+  padding: 0;
 }
 </style>
